84bc8a016425ca86af56d3fed7a198c8834e9a3c
Crimson8230 <pernthaler.hannes@gmail.com>
2026-04-26 01:25:28 +0200

    feat: added spring boot stuff (#5)
    * feat: added mvn spring boot project
    
    used https://start.spring.io/ its awesome
    
    * feat: move spring boot files, configure db, add gitignore *.lst

---
+++ src/main/java/at/mci/sw2/bug_tracking_api/BugTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package at.mci.sw2.bug_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackingApiApplication.class, args);
+	}
+
+}
