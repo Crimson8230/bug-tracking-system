db06d952c98cc5b23eb013758f2db633ae27ee6e
Simon Wabnig <simon.wabnig@gmail.com>
2026-05-09 10:28:10 +0200

    fix: add TicketStatus  IN_ANALYSIS and CANCELLED to align with SRS

--- backend/src/main/java/at/mci/sw2/bug_tracking_api/ticket/TicketStatus.java
+++ backend/src/main/java/at/mci/sw2/bug_tracking_api/ticket/TicketStatus.java
@@ -2,6 +2,6 @@ package at.mci.sw2.bug_tracking_api.ticket;
 
 public class TicketStatus {
     public enum Status {
-        OPEN, IN_PROGRESS, DONE
+        OPEN, IN_ANALYSIS, IN_PROGRESS, DONE, CANCELLED
     }
 }
