3e839fff4cfd74029dfa05f0dd6634236266d3b2
Crimson8230 <pernthaler.hannes@gmail.com>
2026-05-21 23:15:05 +0200

    Update TicketRepository.java

--- backend/src/main/java/at/mci/sw2/bug_tracking_api/ticket/TicketRepository.java
+++ backend/src/main/java/at/mci/sw2/bug_tracking_api/ticket/TicketRepository.java
@@ -2,5 +2,9 @@ package at.mci.sw2.bug_tracking_api.ticket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    Optional<Ticket> findByTitle(String title);
 }
